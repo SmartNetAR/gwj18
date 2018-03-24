669d7e6dbcc1bedb7e1524d52a696b8546b5ce01
Leonardo Casales <leonardo@smartnet.com.ar>
2018-03-24 12:57:24 -0300

    agregado de imputs multiplayer, metafiles, cambio de funcion run

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,31 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	public float Speed;
+	public int m_Id;
+	public float m_Speed;
 
 	private Rigidbody2D rb2D;
 	private bool grounded = false;
 
+	private string m_MovementAxisName;
+	private string m_JumpName;
+
 	void Start() {
+		m_MovementAxisName = "Horizontal" + m_Id;
+		m_JumpName = "Jump" + m_Id;
 		rb2D = GetComponent<Rigidbody2D> ();
 	}
 
-	void Update () {
+	void FixedUpdate () {
+		RunAxis ();
+		Jump ();
+
+	}
+
+	void RunAxis() {
+		float horizontalMovment = Input.GetAxis (m_MovementAxisName);
+			if (GetComponent<SpriteRenderer>().flipX) {
+				GetComponent<SpriteRenderer> ().flipX = false;
+			}
+			transform.Translate ( horizontalMovment * m_Speed * Time.deltaTime, 0, 0) ;
+
+		/* 
+		if ( Input.GetAxis("Horizontal") < -0.1f ) {
+			if (!GetComponent<SpriteRenderer>().flipX) {
+				GetComponent<SpriteRenderer> ().flipX = true;
+			}
+			transform.Translate ( -0.1f * m_Speed, 0, 0) ;
+		}
+		*/
+	}
+
+	void RunGetKey() {
 		if (Input.GetKey(KeyCode.RightArrow) )			{
 			if (GetComponent<SpriteRenderer>().flipX) {
 				GetComponent<SpriteRenderer> ().flipX = false;
 			}
-			transform.Translate ( 0.1f * Speed, 0, 0) ;
+			transform.Translate ( 0.1f * m_Speed, 0, 0) ;
 		}
 		if (Input.GetKey(KeyCode.LeftArrow) ) {
 			if (!GetComponent<SpriteRenderer>().flipX) {
 				GetComponent<SpriteRenderer> ().flipX = true;
 			}
-			transform.Translate ( -0.1f * Speed, 0, 0) ;
+			transform.Translate ( -0.1f * m_Speed, 0, 0) ;
 		}
-		if ( Input.GetAxis("Jump") > 0f ) {
+	}
+
+	void Jump() {
+		if ( Input.GetAxis(m_JumpName) > 0f ) {
 			rb2D.velocity = new Vector2 (rb2D.velocity.x, 10f);
 		}
 	}
+
 	
 }

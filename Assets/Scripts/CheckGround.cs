cafffa0bc05cdfe4d7a31aabe015f95600b42136
TALLER\Leo <leonardo@smartnet.com.ar>
2018-04-06 22:49:42 -0300

    seguimiento de players

---
+++ Assets/Scripts/CheckGround.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour {
+
+	private PlayerController playerController;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		playerController = GetComponentInParent<PlayerController> ();
+		rb2d = GetComponentInParent<Rigidbody2D> ();
+	}
+
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		/*if (collision.gameObject.CompareTag ( "Platform" )) {
+			rb2d.velocity = new Vector3 (0f, 0f, 0f);
+			playerController.transform.parent = collision.transform;
+			playerController.ItsOnTheGround (true);
+		}*/
+	}
+
+	void OnCollisionStay2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag ( "Ground" )) {
+			playerController.ItsOnTheGround (true);
+		}
+		/*if (collision.gameObject.CompareTag ( "Platform" )) {
+			//playerController.transform.parent = collision.transform;
+			playerController.ItsOnTheGround (true);
+		}*/
+	}
+
+	void OnCollisionExit2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag ( "Ground" )) {
+			playerController.ItsOnTheGround (false);
+		}
+		/*if (collision.gameObject.CompareTag ( "Platform" )) {
+			playerController.transform.parent = null;
+			playerController.ItsOnTheGround (false);
+		}*/
+	}
+}
